a08c9e06780f5d8254dd9ccd392ebabdf153eafb
office-user <153056648+office-user@users.noreply.github.com>
2024-07-30 07:03:53 +0530

    add gitignore file

--- src/com/gagan/main/Main.java
+++ src/com/gagan/main/Main.java
@@ -17,81 +17,6 @@ public class Main {
         // Create an instance of the DataBase class
         DataBase db = new DataBase();
 
-//        // Use default configuration for MySQL
-//        try {
-//            db.getOIMConnection(null, DatabaseType.MYSQL); // Will use default values for DB1
-//
-//            // Example query to execute
-//            String query = "SELECT * FROM users";
-//            ResultSet resultSet = db.executeQuery(null, DatabaseType.MYSQL, query); // Will use default values for DB1
-//
-//            // Process the result set
-//            while (resultSet.next()) {
-//                LOG.info(String.format("ID: %s, Username: %s, Password Hash: %s, Email: %s, Created At: %s, Updated At: %s",
-//                        resultSet.getString("id"), resultSet.getString("username"), resultSet.getString("password_hash"),
-//                        resultSet.getString("email"), resultSet.getString("created_at"), resultSet.getString("updated_at")));
-//            }
-//
-//        } catch (SQLException e) {
-//            LOG.log(Level.SEVERE, "SQL Exception occurred", e);
-//        } finally {
-//            // Close the resources
-//            db.close();
-//        }
-
-//        // Use default configuration for ORACLE SQL Developer
-//        try {
-//            db.getOIMConnection(null, DatabaseType.ORACLE); // Will use default values for DB2
-//
-//            // Example query to execute
-//            String query = "SELECT * FROM users";
-//            ResultSet resultSet = db.executeQuery(null, DatabaseType.ORACLE, query); // Will use default values for DB2
-//
-//            // Process the result set
-//            while (resultSet.next()) {
-//                LOG.info(String.format("ID: %s, Username: %s, Password Hash: %s, Email: %s, Created At: %s, Updated At: %s",
-//                        resultSet.getString("id"), resultSet.getString("username"), resultSet.getString("password_hash"),
-//                        resultSet.getString("email"), resultSet.getString("created_at"), resultSet.getString("updated_at")));
-//            }
-//        } catch (SQLException e) {
-//            LOG.log(Level.SEVERE, "SQL Exception occurred", e);
-//        } finally {
-//            // Close the resources
-//            db.close();
-//        }
-
-//        // Use manual configuration for custom database
-//        DBConfig manualDBConfig = new DBConfig(
-//                DatabaseType.MANUAL,
-//                "jdbc:mysql://localhost:3306/mydatabase?ssl-mode=REQUIRED",
-//                "root", // Database User
-//                "", // Database Password
-//                "com.mysql.cj.jdbc.Driver"  // JDBC Driver Class Name
-//        );
-//
-//        try {
-//            db.getOIMConnection(manualDBConfig, DatabaseType.MANUAL); // Will use custom values for DB2
-//
-//            // Example query to execute
-//            String query = "SELECT * FROM users";
-//            ResultSet resultSet = db.executeQuery(manualDBConfig, DatabaseType.MANUAL, query); // Will use custom values for DB2
-//
-//
-//            // Process the result set
-//            while (resultSet.next()) {
-//                LOG.info(String.format("ID: %s, Username: %s, Password Hash: %s, Email: %s, Created At: %s, Updated At: %s",
-//                        resultSet.getString("id"), resultSet.getString("username"), resultSet.getString("password_hash"),
-//                        resultSet.getString("email"), resultSet.getString("created_at"), resultSet.getString("updated_at")));
-//            }
-//
-//        } catch (SQLException e) {
-//            LOG.log(Level.SEVERE, "SQL Exception occurred", e);
-//        } finally {
-//            // Close the resources
-//            db.close();
-//        }
-
-
         // Use custom configuration for a manual database
         DBConfig customDBConfig = new DBConfig(
                 "jdbc:mysql://localhost:3306/mydatabase?ssl-mode=REQUIRED",
@@ -120,27 +45,6 @@ public class Main {
             db.close();
         }
 
-
-        // Use default configuration for Database 2
-        try {
-            db.getOIMConnection(null, DatabaseType.MYSQL); // Will use default values for DB2
-
-            // Example query to execute
-            String query = "SELECT * FROM users";
-            ResultSet resultSet = db.executeQuery(query); // Will use default values for DB2
-
-            // Process the result set
-            while (resultSet.next()) {
-                LOG.info(String.format("ID: %s, Username: %s, Password Hash: %s, Email: %s, Created At: %s, Updated At: %s",
-                        resultSet.getString("id"), resultSet.getString("username"), resultSet.getString("password_hash"),
-                        resultSet.getString("email"), resultSet.getString("created_at"), resultSet.getString("updated_at")));
-            }
-        } catch (SQLException e) {
-            LOG.log(Level.SEVERE, "SQL Exception occurred", e);
-        } finally {
-            // Close the resources
-            db.close();
-        }
     }
 }
 
